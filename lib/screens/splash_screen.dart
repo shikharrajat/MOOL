91bf2f9c7375eea52a5b519018cc7144f756615b
Shikhar <shikharsharma1408@gamil.com>
2023-04-17 18:45:09 +0530

    firebase configured

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  final bool isLogin;
+  const SplashScreen(this.isLogin, {super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 200,
+              width: 150,
+              child: Image.asset('assets/images/union.png'),
+            ),
+            Text(
+              'WellBe',
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w600,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
